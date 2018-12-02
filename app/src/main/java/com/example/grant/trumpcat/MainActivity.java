d36194d7017cc139d96f672e4d2eff44265359fc
gurgenz221 <aritron2@illinois.edu>
2018-12-01 20:25:43 -0600

    --Many errors.

--- app/src/main/java/com/example/grant/trumpcat/MainActivity.java
+++ app/src/main/java/com/example/grant/trumpcat/MainActivity.java
@@ -28,24 +28,29 @@ import com.google.gson.JsonPrimitive;
 public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetJavascriptEnabled")
     //@Override
-    private static RequestQueue requestQueue;
+    private RequestQueue requestQueue;
     private static final String TAG = "TrumpCat";
-    private JSONObject resp;
+    private JSONObject resp = new JSONObject();
+    protected boolean b = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
-        final TextView tv1 = (TextView)findViewById(R.id.statusText);
+        final TextView tv1 = findViewById(R.id.statusText);
         //tv1.setText("");
 
         final Button button = findViewById(R.id.sendButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                EditText getNumber= (EditText) findViewById(R.id.phoneNumber);
+                EditText getNumber= findViewById(R.id.phoneNumber);
                 String number = getNumber.getText().toString();
                 Log.d(TAG, "onClick: number entered is " + number);
                 try {
                     startAPICall("https://api.tronalddump.io/random/quote");
+                    while (!b);
+                    Log.d(TAG, resp.toString());
                     String quote = JsonParse(resp, "value");
                     Log.d(TAG, quote + " downloaded correctly");
                     startAPICall("https://aws.random.cat/meow");
@@ -62,36 +67,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    void startAPICall(String setUrl) throws Exception {
-        try {
-            resp = new JSONObject();
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                setUrl,
-                null,
-                new Response.Listener<JSONObject>() {
+    JSONObject startAPICall(String setUrl) throws Exception {
+
+            try {
+                //JSONObject resp = new JSONObject();
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        setUrl,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                Log.d(TAG, "startAPI onResponse" + response.toString());
+                                resp = response;
+                                b = true;
+                            }
+                        }, new Response.ErrorListener() {
                     @Override
-                    public void onResponse(final JSONObject response) {
-                        Log.d(TAG, "startAPI onResponse" + response.toString());
-                        resp = response;
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    Log.w(TAG, "startAPI onErrorResponse" + error.toString());
-                }
-            });
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.d(TAG, "Catch block in startAPICall");
-            throw e;
-        }
+                    public void onErrorResponse(final VolleyError error) {
+                        Log.w(TAG, "startAPI onErrorResponse" + error.toString());
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+                return resp;
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "Catch block in startAPICall");
+                throw e;
+            }
+
     }
     String JsonParse(JSONObject jason, String key) throws JSONException {
        // JsonParser parser = new JsonParser();
         try {
             String extracted = jason.getString(key);
+            Log.d(TAG, extracted);
             return extracted;
         }
         catch (JSONException e) {
