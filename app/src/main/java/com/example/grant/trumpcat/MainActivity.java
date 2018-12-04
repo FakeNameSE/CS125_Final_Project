f4781416e2f7d21e62486612848ef8d9560e3172
Grant Garrett-Grossman <grantlycee@gmail.com>
2018-12-04 17:57:48 -0600

    Got API stuff working when we want it to!

--- app/src/main/java/com/example/grant/trumpcat/MainActivity.java
+++ app/src/main/java/com/example/grant/trumpcat/MainActivity.java
@@ -24,20 +24,26 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 
+import java.util.regex.*;
+import android.net.Uri;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetJavascriptEnabled")
     //@Override
     private RequestQueue requestQueue;
+    private String catLink = null;
+    private String trumpQuote = null;
+
+    private TextView tv1 = null;
     private static final String TAG = "TrumpCat";
     private JSONObject resp = new JSONObject();
-    protected boolean b = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
-        final TextView tv1 = findViewById(R.id.statusText);
+        tv1 = findViewById(R.id.statusText);
         //tv1.setText("");
 
         final Button button = findViewById(R.id.sendButton);
@@ -47,56 +53,105 @@ public class MainActivity extends AppCompatActivity {
                 EditText getNumber= findViewById(R.id.phoneNumber);
                 String number = getNumber.getText().toString();
                 Log.d(TAG, "onClick: number entered is " + number);
-                try {
-                    startAPICall("https://api.tronalddump.io/random/quote");
-                    while (!b);
-                    Log.d(TAG, resp.toString());
-                    String quote = JsonParse(resp, "value");
-                    Log.d(TAG, quote + " downloaded correctly");
-                    startAPICall("https://aws.random.cat/meow");
-                    String imageLink = JsonParse(resp, "file");
-                    Log.d(TAG, imageLink + " downloaded correctly");
-
-                } catch(Exception e) {
-                    Log.d(TAG, e.toString());
-                    tv1.setText("Content Download Failed :(");
+                // Thank you Geeks for geeks!
+                // Find matching between given number and regular expression, to detect obviously
+                // wrong numbers.
+                /*
+                Pattern p = Pattern.compile("(0/91)?[7-9][0-9]{9}");
+                Matcher m = p.matcher(number);
+                if (!(m.find() && m.group().equals(number))) {
+                    Log.w(TAG, "onClick: number entered is invalid");
+                    tv1.setText("Invalid phone number :(");
                     return;
                 }
-                tv1.setText("Sent!");
+                */
+                startAPICalls();
             }
         });
 
     }
-    JSONObject startAPICall(String setUrl) throws Exception {
 
-            try {
-                //JSONObject resp = new JSONObject();
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                        Request.Method.GET,
-                        setUrl,
-                        null,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(final JSONObject response) {
-                                Log.d(TAG, "startAPI onResponse" + response.toString());
-                                resp = response;
-                                b = true;
+    private void sendSMS() {
+        if (trumpQuote == null || catLink == null) {
+            Log.d(TAG, "Content Download Failed :(");
+            tv1.setText("Content Download Failed :(");
+            return;
+        }
+        tv1.setText("Sent!");
+    }
+
+    private void startAPICalls() {
+        trumpAPI();
+    }
+
+    private void trumpAPI() {
+        try {
+            //JSONObject resp = new JSONObject();
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.tronalddump.io/random/quote",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, "startAPI onResponse" + response.toString());
+                            try {
+                                trumpQuote = JsonParse(response, "value");
+                            } catch (JSONException e) {
+                                Log.e(TAG, e.toString());
                             }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(final VolleyError error) {
-                        Log.w(TAG, "startAPI onErrorResponse" + error.toString());
-                    }
-                });
-                requestQueue.add(jsonObjectRequest);
-                return resp;
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.d(TAG, "Catch block in startAPICall");
-                throw e;
-            }
+                            catAPI();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, "startAPI onErrorResponse" + error.toString());
+                    catAPI();
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Catch block in startAPICall");
+            throw e;
+        }
 
     }
+
+    private void catAPI() {
+        try {
+            //JSONObject resp = new JSONObject();
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://aws.random.cat/meow",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, "startAPI onResponse" + response.toString());
+                            try {
+                                catLink = JsonParse(response, "file");
+                            } catch (JSONException e) {
+                                Log.e(TAG, e.toString());
+                            }
+                            sendSMS();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, "startAPI onErrorResponse" + error.toString());
+                    sendSMS();
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Catch block in startAPICall");
+            throw e;
+        }
+
+    }
+
     String JsonParse(JSONObject jason, String key) throws JSONException {
        // JsonParser parser = new JsonParser();
         try {
